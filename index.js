ae4dbbafb260effd45e59e6b6b764b9ec5099164
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-10 15:44:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ class Polygon{
 
 class Triangle extends Polygon{
   get isValid(){
+    const points = this.points;
     const firstSideValid = points[0] + points[1] > points[2]
     const secondSideValid = points[0] + points[2] > points[1]
     const thirdSideValid = points[1] + points[2] > points[0]
