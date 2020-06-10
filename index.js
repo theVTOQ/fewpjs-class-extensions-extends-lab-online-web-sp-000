a8318e2d45afc8fb8f44fc3bf7be19baefe7174c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-06-10 15:49:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+class Polygon{
+  constructor(points){
+    this.points = points;
+  }
+
+  get countSides(){
+    return this.points.length;
+  }
+
+  get perimeter(){
+    return this.points.reduce((sum, currentValue) => sum + currentValue, 0);
+  }
+}
+
+class Triangle extends Polygon{
+  get isValid(){
+    const points = this.points;
+    const firstSideValid = points[0] + points[1] > points[2]
+    const secondSideValid = points[0] + points[2] > points[1]
+    const thirdSideValid = points[1] + points[2] > points[0]
+
+    return firstSideValid && secondSideValid && thirdSideValid;
+  }
+}
+
+class Square extends Polygon{
+  get isValid(){
+    const points = this.points;
+    return points[0] === this.area/points[0];
+  }
+
+  get area(){
+    const points = this.points;
+    return points[0] * points[1];
+  }
+}
